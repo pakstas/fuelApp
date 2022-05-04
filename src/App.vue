9256e0d07057edbe1cab07b3965493dd3cc1e437
Pakstas <pakstas@gmail.com>
2022-05-04 16:46:44 +0300

    first commit, added Header

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="main">
+        <Header />
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+body {
+  background-image: url("./assets/img/bg_car.jpg");
+  background-position: center center;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+<style scoped>
+.container > .main {
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  background: rgb(255, 255, 255, 0.8);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div class="nav">
+        <img src="../assets/img/Fuel_Logo.png" alt="Company logo" />
+
+        <nav>
+          <span>
+            <router-link to="/register">Register</router-link>
+            <router-link to="/login">Login</router-link>
+          </span>
+        </nav>
+      </div>
+      <div class="page-title">
+        <h1 class="title">{{ this.$route.name }}</h1>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px;
+  background: rgb(147, 224, 243);
+  border-radius: 0px 0px 5px 5px;
+}
+
+.nav img {
+  height: 40px;
+}
+
+.nav a {
+  text-decoration: none;
+  color: rgb(0, 0, 0);
+  margin-right: 5px;
+  margin-left: 10px;
+}
+.nav a:hover {
+  text-decoration: underline;
+}
+
+.nav a:last-child {
+  margin-right: 0px;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+.wrapper .page-title {
+  height: 200px;
+  padding: 5px;
+  display: flex;
+  align-items: center;
+}
+</style>
