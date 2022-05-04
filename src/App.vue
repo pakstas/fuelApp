dcca82a2bc5fabc36e809d96fe3380483a88c711
Pakstas <pakstas@gmail.com>
2022-05-04 16:55:54 +0300

    added Footer component

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,19 @@
         <Header />
         <router-view />
       </div>
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hero">
+    <div class="hero-foot">
+      <span class="is-size-7 has-text-light"> Copyright @ 2022 m. </span>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.hero {
+  height: 50px;
+  background-color: rgb(0, 0, 0, 0.8);
+}
+
+.hero-foot {
+  padding: 12.5px;
+  text-align: center;
+}
+</style>
